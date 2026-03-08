70c95b7847871a566fa9327512bb52d3e01b9223
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2026-03-08 13:24:24 +0100

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Jakobs react wiii</h1>
+      <h1>Jakobs react wo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
