08f36fdc6c666dc7cb1fa25507fb96918719c5f7
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2026-03-08 13:02:07 +0100

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,35 +1,8 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     globals: true, // OBS: plural 'globals', inte 'global'
-//     environment: "jsdom",
-//     coverage: {
-//       provider: "v8", // snabb coverage
-//       reporter: ["text", "html"], // visa i terminal + html
-//       all: true, // täck alla källfiler
-//       statements: 90,
-//       branches: 90,
-//       functions: 90,
-//       lines: 90,
-//     },
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-    root: ".", // frontend är root
-  build: {
-    outDir: resolve(__dirname, "dist"), // bygg output till frontend/dist
-    emptyOutDir: true,
-  },
   test: {
     globals: true,
     environment: "jsdom",
