6069e74800e948b489788ca2e60637670dbe3c66
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2026-03-08 13:04:39 +0100

    test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -8,7 +8,7 @@ describe("App component", () => {
     render(<App />);
 
     // Huvudrubrik
-    expect(screen.getByText("Jakobs react NYA")).toBeInTheDocument();
+    expect(screen.getByText("Jakobs react NYAA")).toBeInTheDocument();
 
     // Bilder
     expect(screen.getByAltText("Vite logo")).toBeInTheDocument();
