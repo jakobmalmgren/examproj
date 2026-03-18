438f9fe3c01caafcfd08c4d8bdadfd53edabddc8
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2026-03-18 10:24:40 +0100

    added some carousel and a review template

--- frontend/pages/LogInSignUp/LogInSignUp.tsx
+++ frontend/pages/LogInSignUp/LogInSignUp.tsx
@@ -1,15 +1,16 @@
 import "./LogInSignUp.css";
-// import AuthSkelleton from "../../components/AuthSkelleton/AuthSkelleton";
+import Carousel from "../../components/Carousel/Carousel";
 import { Outlet } from "react-router-dom";
 const LogInSignUp = () => {
   return (
     <div className="loginSignup">
       <div className="loginSignup__container">
         <div className="loginSignup__form">
-          {/* <AuthSkelleton /> */}
           <Outlet />
         </div>
-        <div className="loginSignup__hero"></div>
+        <div className="loginSignup__hero">
+          <Carousel></Carousel>
+        </div>
       </div>
     </div>
   );
