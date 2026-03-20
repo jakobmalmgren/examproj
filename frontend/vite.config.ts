0376a1e0d22df2c8338eef0d7d11a7d103236c8b
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2026-03-20 14:28:59 +0100

    added the config again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,22 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-//   test: {
-//     globals: true,
-//     environment: "jsdom",
-//     coverage: {
-//       provider: "v8",
-//       reporter: ["text", "html"],
-//       all: true,
-//       // ⚠ här sätter du thresholds
-//       thresholds: {
-//         statements: 90,
-//         branches: 90,
-//         functions: 90,
-//         lines: 90,
-//       },
-//     },
-//   },
-// });
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      all: true,
+      // ⚠ här sätter du thresholds
+      thresholds: {
+        statements: 90,
+        branches: 90,
+        functions: 90,
+        lines: 90,
+      },
+    },
+  },
+});
