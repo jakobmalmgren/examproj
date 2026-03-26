11e264492f9b8c8850f240d5782444ff5e57a6b1
Jakob  Malmgren <jakob.malmgren1987@gmail.com>
2026-03-26 16:20:50 +0100

    took care of some deletingapplication while waiting on access to AWS service email

---
+++ backend/functions/applications/deleteApplication/index.ts
@@ -0,0 +1,51 @@
+import middy from "@middy/core";
+import { client } from "../../../config/dj";
+import { checkAuth } from "../../../middlewares/auth/checkAuth";
+import { DeleteCommand } from "@aws-sdk/lib-dynamodb";
+
+const deleteApplication = async (event) => {
+  const userName = event.user.username.S;
+  const id = event.pathParameters.id;
+  console.log("EVENT!", event);
+
+  try {
+    const command = new DeleteCommand({
+      TableName: "ApplicationsTable",
+      Key: {
+        pk: `USERNAME#${userName}`,
+        sk: `APPLICATION#${id}`,
+      },
+    });
+
+    await client.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        success: true,
+        message: "Application deleted succesfully",
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        message: error.message,
+      }),
+    };
+  }
+};
+
+export const handler = middy(deleteApplication)
+  .use(checkAuth())
+  .onError((request) => {
+    request.response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        success: false,
+        message: "Input validation failed",
+        details: request.error?.details || request.error?.message,
+      }),
+    };
+  });
